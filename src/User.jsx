000ccf86d78ad1abc18eefa8f5fb3f6403ac066a
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-04-28 19:15:53 +0100

    initial styling

--- src/User.jsx
+++ src/User.jsx
@@ -1,49 +1,63 @@
-import React, {Component} from "react";
-import './User.css';
+import React, { Component } from "react";
+import "./User.css";
 class User extends Component {
-   
-    // Constructor 
-    constructor(props) {
-        super(props);
-   
-        this.state = {
-            users: [],
-            getUserInfo: false
-        };
-    }
-   
-     
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data) // this confirmed that data is collected
-                this.setState({
-                    users: data,
-                    getUserInfo: true
-                });
-            })
-    }
-    render() {
-        const { getUserInfo, users } = this.state;
-        if (getUserInfo === false) return 
-            <p> Internet connection is require to load your request.</p>;
-        
-        return (
-        <div className = "App">
-            <h1> Fetch data from an api in react </h1>  {
-                users.map((user) => (
-                <ol key = { user.id } >
-                    User_Name: { user.username }, 
-                    Full_Name: { user.name }, 
-                    User_Email: { user.email }
-                    User_address:  { user.address.street }
-                    </ol>
-                ))
-            }
-        </div>
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      getUserInfo: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data); // this confirmed that data is collected
+        this.setState({
+          users: data,
+          getUserInfo: true,
+        });
+      });
+  }
+  render() {
+    const { getUserInfo, users } = this.state;
+    if (getUserInfo === false) return;
+    <p> Internet connection is require to load your request.</p>;
+
+    return (
+      <div className="App">
+        {users.map((user) => (
+          <div key={user.id} className="card">
+            <h1 className="font-family"> {user.name} </h1>
+            <p className="decr"> {user.company.catchPhrase} </p>
+
+            <h3 className="gen-info"> General Info </h3>
+            <div className="d-flex">
+              <div>
+                <b>Address</b>
+              </div>
+              <div> {user.address.street} </div>
+            </div>
+            <div className="d-flex">
+              <div>
+                <b>Email</b>
+              </div>
+              <div> {user.email} </div>
+            </div>
+            <div className="d-flex">
+              <div>
+                <b>Phone</b>
+              </div>
+              <div> {user.phone} </div>
+            </div>
+          </div>
+        ))}
+      </div>
     );
+  }
 }
-}
-   
+
 export default User;
